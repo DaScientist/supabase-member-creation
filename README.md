e8dea14616d82cb9da448d3fc4e96de30bd00557
Mustanseer Sakerwala <connect.mustan@gmail.com>
2022-05-24 22:49:40 +0530

    Completed the migration! Muhahahaha!!😁😁

---
+++ README.md
@@ -0,0 +1,18 @@
+## Automated Supabase Auth user creation from CSV file
+
+- To automatically generate users of supabase from a csv file containing their passwords:
+  1. Place the CSV file in the `files` folder
+  2. Create the file: `types/index.d.ts` and create a type for User containing the keys for the csv object. Example illustrated below
+  3. Create `.env` file containing following values:
+
+```
+NEXT_PUBLIC_SUPABASE_URL # Available on Supabase Auth Settings Page
+NEXT_PUBLIC_SUPABASE_ANON_KEY # Available on Supabase Auth Settings Page
+FILE_PATH # Path to your csv file
+```
+- Edit the data obj in `supabase.auth.singUp` function according to your needs
+- Run `yarn start`
+
+
+[Note: Example User is given in comment at the top in index.ts]
+##### Congrats for completing the automated task!
--- index.ts
+++ index.ts
@@ -1,6 +1,81 @@
-import * as dotenv from 'dotenv';
+/** 
+ * type User = { 
+ * 	its: string, 
+ * 	email: string, 
+ * 	password: string 
+ * }
+*/
+
+import * as dotenv from "dotenv";
 import { createClient } from "@supabase/supabase-js";
+import * as fs from "fs";
+import Papa from "papaparse";
 
-dotenv.config()
+dotenv.config();
 
-console.log(process.env.MUSTAN);
+const supabaseUrl: string = process.env.NEXT_PUBLIC_SUPABASE_URL
+  ? process.env.NEXT_PUBLIC_SUPABASE_URL
+  : "";
+const supabaseAnonKey: string = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  ? process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  : "";
+if (process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY === "")
+  console.error("Error: env not accessible");
+
+const csvFilePath: string = process.env.FILE_PATH ? process.env.FILE_PATH : ""
+// console.log(csvFilePath);
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+
+async function signIn(member: User): Promise<void> {
+  const { user, session, error } = await supabase.auth.signUp(
+    {
+      email: member.email,
+      password: member.password,
+    },
+    { // Modify this data according to your needs for additional data in the user object
+      data: {
+        its: member.its,
+      },
+    }
+  );
+  console.log(
+    "-----------------------------------------Error-----------------------------------------"
+  );
+  console.log(error);
+  console.log(
+    "----------------------------------------Session----------------------------------------"
+  );
+  console.log(session);
+  console.log(
+    "-----------------------------------------User------------------------------------------"
+  );
+  console.log(user);
+}
+
+
+const readCSV = async (filePath: string): Promise<User[]> => {
+  const csvFile = fs.readFileSync(filePath);
+  const csvData = csvFile.toString();
+  return new Promise((resolve) => {
+    Papa.parse(csvData, {
+      header: true,
+      transformHeader: (header) => header.trim(),
+      complete: (results: Papa.ParseResult<User>) => {
+        console.log("Complete", results.data.length, "records.");
+        resolve(results.data);
+      },
+    });
+  });
+};
+
+const test = async () => {
+  let parsedData: User[] = await readCSV(csvFilePath);
+  await Promise.all(parsedData.map(async (member: User) => {
+		await signIn(member);
+	}));
+
+};
+
+// test();
